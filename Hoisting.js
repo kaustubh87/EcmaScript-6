dc70c1acb16ed7d3dfe7e20598b4b6991bd900d7
kvin1087 <kaustubh87@gmail.com>
2017-11-09 21:43:58 -0600

    Declare variables before calling functions

--- Hoisting.js
+++ Hoisting.js
@@ -1,9 +1,17 @@
 
-let age;
-age = 27;
-console.log(age);
+//let age;
+//age = 27;
+//console.log(age);
 
 //var age;  // O/P - 27
 
 //let age;  // Error Reference error as age is not defined.
+
+function doSomething(){
+    age = 27;
+}
+
+let age;
+doSomething();
+console.log(age); // Declare things before using them
 
