4c79e50e34b8982ac787ed3696c31cbc21e70762
kvin1087 <kaustubh87@gmail.com>
2018-06-26 11:36:55 -0400

    let and const in es6

---
+++ variables.js
@@ -0,0 +1,19 @@
+/*
+let name = 'Kaustubh';
+name = 'Kvin';
+
+const person = { data: ['Hello'] };
+
+person.data.push('something');
+
+console.log(person);
+
+console.log(name);
+
+/* Check Block Scoped */
+
+if(1 === 1) {
+    let name = 'Kvin';
+}
+
+console.log(name);
